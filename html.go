5feda2b05e7e89fe7ccc57f71805c035d271ef67
RealColorDream <notrealcolordream@gmail.com>
2024-09-30 21:16:05 +0200

    :white_check_mark: :heavy_plus_sign: :construction: added function to serve a webpage using gin + added dependecies for gin / http

--- html.go
+++ html.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	_ "github.com/gin-gonic/gin"
+	"net/http"
+)
+import _ "net/http"
+
+func server() {
+	router := gin.Default()
+	var data = readData("response_body.json")
+
+	router.GET("/games", func(c *gin.Context) {
+		c.JSON(http.StatusOK, data)
+	})
+	router.Run(":8080")
+}
